fd00284459f420c28fef9f061dc8e8197ba6fdc4
Michal Charemza <michal@charemza.name>
2019-08-05 12:37:42 +0100

    (tests) More robust termination of application

--- test.py
+++ test.py
@@ -10,14 +10,16 @@ import requests
 class TestS3Proxy(unittest.TestCase):
 
     def test_root(self):
-        stop_application = create_application(8080)
+        wait_until_started, stop_application = create_application(8080)
         self.addCleanup(stop_application)
+
+        wait_until_started()
 
         response = requests.get('http://127.0.0.1:8080/')
         self.assertEqual(response.content, b'Hello World!')
 
 
-def create_application(port):
+def create_application(port, max_attempts=100):
     process = subprocess.Popen(
         ['python3', 'app.py', ],
         env={
@@ -26,18 +28,18 @@ def create_application(port):
         }
     )
 
-    max_attempts = 100
-    for i in range(0, max_attempts):
-        try:
-            with socket.create_connection(('127.0.0.1', port), timeout=0.1):
-                break
-        except OSError:
-            if i == max_attempts - 1:
-                raise
-            time.sleep(0.01)
+    def wait_until_started():
+        for i in range(0, max_attempts):
+            try:
+                with socket.create_connection(('127.0.0.1', port), timeout=0.1):
+                    break
+            except OSError:
+                if i == max_attempts - 1:
+                    raise
+                time.sleep(0.01)
 
     def stop():
         process.terminate()
         process.wait(timeout=5)
 
-    return stop
+    return wait_until_started, stop
