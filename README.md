9996e661c05b43a41fdd72724b91c4ca0c61dc7d
Michal Charemza <michal@charemza.name>
2019-08-05 15:14:53 +0100

    (feat) Simple S3 proxy

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # s3proxy [![CircleCI](https://circleci.com/gh/uktrade/s3proxy.svg?style=svg)](https://circleci.com/gh/uktrade/s3proxy) [![Test Coverage](https://api.codeclimate.com/v1/badges/80938f6b27356411efd5/test_coverage)](https://codeclimate.com/github/uktrade/s3proxy/test_coverage)
 
 An OAuth-authenticated streaming proxy to S3
+
+
+## Running tests
+
+```bash
+./minio-start.sh  # Only required once
+./test.sh
+```
--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-import os
-import signal
-
 from gevent import (
     monkey
 )
-from gevent.pywsgi import (
-    WSGIServer,
+monkey.patch_all()
+
+import boto3
+from botocore.client import (
+    Config,
 )
 from flask import (
     Flask,
 )
+from gevent.pywsgi import (
+    WSGIServer,
+)
 
-monkey.patch_all()
+import os
+import signal
+
+
+s3 = boto3.client(
+    's3',
+    endpoint_url=os.environ['AWS_S3_ENDPOINT'],
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    config=Config(signature_version='s3v4'),
+    region_name='us-east-1',
+)
+bucket = os.environ['AWS_S3_BUCKET']
 app = Flask('app')
 
 
 @app.route('/<path:path>')
-def entry_point(path):
-    return path
+def proxy(path):
+    obj = s3.get_object(
+        Bucket=bucket,
+        Key=path,
+    )
+    return obj['Body'].read()
 
 
 if __name__ == '__main__':
