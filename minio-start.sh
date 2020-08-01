c38ac31b313753f17e2e88561faf6461d8ab7daa
Michal Charemza <michal@charemza.name>
2020-08-01 08:57:58 +0100

    test: bump minio to have more S3 Select support

--- minio-start.sh
+++ minio-start.sh
@@ -7,5 +7,5 @@ docker run --rm -p 9000:9000 --name s3proxy-minio -d \
   -e 'MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' \
   -e 'MINIO_REGION=us-east-1' \
   --entrypoint sh \
-  minio/minio:RELEASE.2019-08-01T22-18-54Z \
+  minio/minio:RELEASE.2020-07-31T03-39-05Z \
   -c 'mkdir -p /data/my-bucket && minio server /data'
