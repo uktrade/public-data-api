016af6520b1238463f311c84e3072ef71140355c
Michal Charemza <michal@charemza.name>
2021-12-08 15:20:25 +0000

    ci: bump minio in tests
    This is an attempt to support deletion of folders in tests more easily. I don't
    think it does, but we should bump minio anyway.

--- start-services.sh
+++ start-services.sh
@@ -9,7 +9,7 @@ docker run --rm -p 9000:9000 --name s3proxy-minio -d \
   -e 'MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' \
   -e 'MINIO_REGION=us-east-1' \
   --entrypoint sh \
-  minio/minio:RELEASE.2020-07-31T03-39-05Z \
+  minio/minio:RELEASE.2021-11-24T23-19-33Z.hotfix.1d85a4563 \
   -c 'mkdir -p /data1 && mkdir -p /data2 && mkdir -p /data3 && mkdir -p /data4 && minio server /data{1...4}'
 
 docker run --network public-data-api-network --rm -p 9201:9200 -p 9301:9300 --name elasticsearch -d \
