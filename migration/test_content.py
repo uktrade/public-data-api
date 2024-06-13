dd6e4cc01785d6d4559efbb6f1de716115380656
Michal Charemza <michal@charemza.name>
2024-06-13 12:09:11 +0000

    param

--- migration/test_content.py
+++ migration/test_content.py
@@ -9,9 +9,14 @@ import hashlib
 import httpx
 import pytest
 
-def test():
-    url = 'https://data.api.trade.gov.uk/v1/datasets/uk-tariff-2021-01-01/versions/v4.0.246/data?format=sqlite&download'
-    expected_digest = '874f3ad4522e250b2fb55acc43fc7d4205a002dea239d7154a3c99ee59b65fe9'
+@pytest.mark.parametrize(
+    'url, expected_digest', (
+    (
+        'https://data.api.trade.gov.uk/v1/datasets/uk-tariff-2021-01-01/versions/v4.0.246/data?format=sqlite&download',
+        '874f3ad4522e250b2fb55acc43fc7d4205a002dea239d7154a3c99ee59b65fe9',
+    ),
+))
+def test(url, expected_digest):
     m = hashlib.sha256()
     with httpx.stream('GET', url) as r:
         for chunk in r.iter_bytes():
